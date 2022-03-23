56f7a74a33d40d17b1cfb410ee6e5cb0d34de8f1
sergioj97 <sergiojsancam@gmail.com>
2022-03-23 16:48:05 +0100

    Primer commit

---
+++ src/main/java/es/urjccode/correointerno/CorreoInternoApplication.java
@@ -0,0 +1,13 @@
+package es.urjccode.correointerno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreoInternoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreoInternoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/urjccode/correointerno/CorreoInternoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjccode.correointerno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreoInternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
